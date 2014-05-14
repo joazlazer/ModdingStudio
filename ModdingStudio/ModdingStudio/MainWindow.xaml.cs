439679ffd2ba87cd6446bf1bb06b18ab026d470e
joazlazer <joazlazer@hotmail.com>
2014-05-14 16:01:00 -0400

    Window Styling
    Making it look a bit more like VS13.

--- ModdingStudio/ModdingStudio/MainWindow.xaml.cs
+++ ModdingStudio/ModdingStudio/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ModdingStudio;
 
 namespace ModdingStudio.Application
 {
